b4ba11b6739718d42fb3f7402ecf447336b3548f
Guaracy A. Lima <guaracyaraujolima@gmail.com>
2024-12-11 01:01:19 -0300

    base project

---
+++ src/main/java/br/com/zth/rateyou/RateyouApplication.java
@@ -0,0 +1,13 @@
+package br.com.zth.rateyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateyouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateyouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zth/rateyou/RateyouApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zth.rateyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateyouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
